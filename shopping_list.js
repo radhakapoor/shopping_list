64b553c1aeaa6da185fc67642ed4662caf87a4da
Adam Shimali <adam.shimali@gmail.com>
2014-12-23 22:17:58 +0000

    Bit of a refactor

--- shopping_list.js
+++ shopping_list.js
@@ -1,61 +1,54 @@
+var clickAddItem = function(event) {
+    var form_input = $('#item').val();
+    var delete_button = '<input type="button" value="delete" class="delete"/>';
+    var item = $('<li>'+form_input+' '+delete_button+'</li>');
+    $('#list').append('<br>');
+    $('#list').append(item);
+    $('#form')[0].reset();
+};
+
+var enterAddItem = function(event) {
+    var form_input = $('#item').val();
+        var delete_button = '<input type="button" value="delete"  class="delete"/>';
+        var code = event.which;
+        var item = $('<li>'+form_input+' '+delete_button+'</li>');
+        if (code==13) {
+            $('#list').append('<br>');
+            $('#list').append(item);
+            $('#form')[0].reset();
+            return false;
+        }
+};
+
+// could merge the above two by handling form submit rather than
+// click on submit button and keyboard enter as distinct events
+
+var deleteItem = function(event) {
+    $(event.currentTarget).parent().wrap('<strike>');
+};
 
 $(document).ready(function() {
-    $('#submit').click(function(){
-    	var form_input = $('#item').val();
-    	var delete_button = '<input type="button" value="delete"/>';
-    	var item = $('<li>'+form_input+' '+delete_button+'</li>');   	  	
-    	$('#list').append('<br>');
-    	$('#list').append(item);	        
-	    $('#form')[0].reset();	    	        
-    })
+    $('#submit').click(clickAddItem);
+    $('#list').on('click', '.delete', deleteItem);
+    $('#form').on('keydown', enterAddItem);
+    $('#item').focus();
+    $('#reset').click(function(){
+        location.reload();
+    });
 });
 
-$(document).ready(function() {
-	$('#form').on('keydown', function(e) {
-		var form_input = $('#item').val();
-		var delete_button = '<input type="button" value="delete"/>';
-		var code = e.which;
-		var item = $('<li>'+form_input+' '+delete_button+'</li>');		 		
-		if (code==13){
-			$('#list').append('<br>');			
-			$('#list').append(item);										
-			$('#form')[0].reset();
-			return false;					
-		}		
-	})
-});
-
-$(document).ready(function() {
-	$('#list').on('click', 'li', function() {
-		$(this).wrap('<strike>');
-	})
-});
-
-$(document).ready(function() {
-	$('#reset').click(function(){
-		location.reload();
-	})
-});
 
 // // #this fires upon click of 'li' element, should really be on click of the deletebutton'
-// $(document).ready(function() {	
+// $(document).ready(function() {
 // 	// var delete_button = $('<input type="button" value="delete"/>');
-// 	$('#list').on('click', 'li', function() {		
+// 	$('#list').on('click', 'li', function() {
 // 		$(this).remove();
 // 	})
 // });
 
 // error if add same item twice
 // slide-up for deleted items
-// slide down for added items 
 
-// $('#clickme').click(function() {
-// 	$('#book').fadeIn('slow', function() {
-// 	});
-// });
-
-
-	
 
 
 
