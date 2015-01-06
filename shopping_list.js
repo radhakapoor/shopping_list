18a44194730a6a39dc50c7f11f4795b1bd484b94
radha kapoor <radha@radhakapoor.net>
2015-01-06 12:29:30 -0500

    added loop to detect duplicates

--- shopping_list.js
+++ shopping_list.js
@@ -1,12 +1,25 @@
+
+var storage = []
+
 var clickAddItem = function(event) {
 	var form_input = $('#item').val();	
 	var purchased_button = '<input type="button" value="purchased" class="purchased"/>';
 	var delete_button = '<input type="button" value="delete" class="delete"/>';
-	var item = $('<li>'+form_input+' '+purchased_button+' '+delete_button+'</li>');
+	var item = $('<li>'+form_input+' '+purchased_button+' '+delete_button+'</li>');	
 	if (form_input === '') {
 		alert('Shopping item field is empty!');
 		return false;	}
-	$('<br>').appendTo('#list');	
+	$('<br>').appendTo('#list');
+	storage.push(form_input);
+	storage.sort();
+	var last = storage[0];
+	for (var i = 1; i < storage.length; i++) {
+		if (storage[i] == last) alert('This item is already on your list: '+last);
+		last = storage[i];
+	}
+	for (var i = 0; i < storage.length; i++) {
+		console.log(storage[i]);
+	}	
 	$(item).appendTo('#list').hide().fadeIn(1500);	
 	$('#form')[0].reset();
 };
@@ -21,8 +34,18 @@ var enterAddItem = function(event) {
 		if (form_input === '') {
 		alert('Shopping item field is empty!');
 		return false;   }
-		$('<br>').appendTo('#list');		
-		$(item).appendTo('#list').hide().fadeIn(1500);		
+		$('<br>').appendTo('#list');
+		storage.push(form_input);
+		storage.sort();
+		var last = storage[0];
+		for (var i = 1; i < storage.length; i++) {
+			if (storage[i] == last) alert('This item is already on your list: '+last);
+			last = storage[i];
+		}
+		for (var i = 0; i < storage.length; i++) {
+		console.log(storage[i]);
+	    }			
+		$(item).appendTo('#list').hide().fadeIn(1500);			
 		$('#form')[0].reset();
 		return false;
 	}
@@ -45,20 +68,16 @@ $(document).ready(function() {
 	$('#reset').click(function(){
 		location.reload();
 	});
-});    
+});  
 
+// checking for duplicate items in array:
+// created a global array to store items
+// write loop to check if there is a duplicate
+// alert if there is a duplicate
 
-// error if add same item twice
 // slide-up for deleted items
 // slide down for added items 
 
-// $('#clickme').click(function() {
-// 	$('#book').fadeIn('slow', function() {
-// 	});
-// });
-
-//checkfor emptry strings
-//local storage for items
 //loop through items to see if repeats
 //change event handler to submit
 
